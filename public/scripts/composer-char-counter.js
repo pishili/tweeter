55715dbbdca662939158711a75aab523231c458b
ladan <pishili@users.noreply.github.com>
2020-01-29 13:17:02 -0800

    fixed the header of article related to container

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,4 +1,4 @@
-const myCalculator = function() {
+const myCalculator = () => {
     const counter = 140 - $("#tweet_text").val().length;
     if (counter >= 0) {
         $("#char_counter").text( 140 - $("#tweet_text").val().length);
@@ -6,7 +6,6 @@ const myCalculator = function() {
         console.log('print red');
     }
 }
-
 
 $(document).ready(function() {
     $("#tweet_text").on('keydown', myCalculator);
