d56d2787be6f04c35dd0d5eb1b54b6d33cfe517b
pishili <pishili@users.noreply.github.com>
2020-02-01 12:58:23 -0800

    Form Submission using JQuery

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,40 +7,41 @@
 // Test / driver code (temporary). Eventually will get this from the server.
 const tweetData = {
     "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png",
         "handle": "@SirIsaac"
-      },
+    },
     "content": {
         "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
+    },
     "created_at": 1461116232227
- }
+}
 
- const data = [
+const data = [
     {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
+        "user": {
+            "name": "Newton",
+            "avatars": "https://i.imgur.com/73hZDYK.png"
+            ,
+            "handle": "@SirIsaac"
+        },
+        "content": {
+            "text": "If I have seen further it is by standing on the shoulders of giants"
+        },
+        "created_at": 1461116232227
     },
     {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
+        "user": {
+            "name": "Descartes",
+            "avatars": "https://i.imgur.com/nlhLi3I.png",
+            "handle": "@rd"
+        },
+        "content": {
+            "text": "Je pense , donc je suis"
+        },
+        "created_at": 1461113959088
     }
-  ]
+]
 
 
 
@@ -75,38 +76,60 @@ const createTweetElement = (tweetObj) => {
 console.log(createTweetElement(tweetData));
 
 
-const renderTweets = (arrTweetObjects) => {
+// const renderTweets = (arrTweetObjects) => {
 
-    arrTweetObjects.forEach(tweetObj => {
-        const tweetMarkup = createTweetElement(tweetObj);
-        const container = $(".container");
-        console.log(container);
-        $( ".container" ).append(tweetMarkup);
-        // $( ".container" ).append("YOOooooooOOOO");
+//     arrTweetObjects.forEach(tweetObj => {
+//         const tweetMarkup = createTweetElement(tweetObj);
+//         const container = $(".container");
+//         console.log(container);
+//         $( ".container" ).append(tweetMarkup);
+//         // $( ".container" ).append("YOOooooooOOOO");
+//     });
+// }
+
+
+// const submitForm = () => {
+
+
+// }
+
+const renderTweets = (data) => {
+    data === Array;
+    data.forEach(() => {
+        // 
     });
-}
+};
 
-
-const submitForm = () => {
-
-
-}
-
-console.log(renderTweets(data));
-
-const loadTweets = function() {
-    // make a request to tweets
-    // recieve the array of tweets as JSON
-    $(function() {
-        const $button = $('#load-more-posts');
-        $button.on('click', function () {
-          console.log('Button clicked, performing ajax call...');
-          $.ajax('more-posts.html', { method: 'GET' })
-          .then(function (morePostsHtml) {
-            console.log('Success: ', morePostsHtml);
-            $button.replaceWith(morePostsHtml);
-          });
+const loadTweets = function () {
+    const $button = $('#load-more-posts');
+    $.ajax('http://localhost:8080/tweets', { method: 'GET' })
+        .then(function (data) {
+            console.log(data);
         });
-      });
-
 }
+
+
+    // (() => {
+    //     const $button = $('#load-more-posts');
+    //     $button.on('click', function () {
+    //         event.preventDefault();
+    //         console.log('Button clicked, performing ajax call...');
+    //         $.ajax('more-posts.html', { method: 'GET' })
+    //             .then(function (morePostsHtml) {
+    //                 console.log('Success: ', morePostsHtml);
+    //                 $button.replaceWith(morePostsHtml);
+    //             });
+    //     });
+    // });
+
+(() => {
+    const $button = $('#tweet-button');
+    const $form = $('#tweet-post-form');
+    $button.on('click', function () {
+        event.preventDefault();
+        console.log('Button clicked, performing ajax call...');
+        console.log($(form).serialize());
+        $.ajax('/tweets', { method: 'POST', data: $(form).serialize() });
+    });
+})
+
