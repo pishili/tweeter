5ae29f3dceeb5ed0e877011b913c9f8e1cd70064
pishili <pishili@users.noreply.github.com>
2020-02-01 14:48:51 -0800

    added createTweetElement function with html manually inside

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,57 +4,48 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const createTweetElement = (tweetObj, moutPoint) => {
-    tweetObj.forEach((tweet) => {
-        const $username = $('<h2>').text(tweet.user.name);
-        const $userhandle = $('<h4>').text(tweet.user.handle);
-        const $tweetContent = $('<p>').text(tweet.content.text);
-        const $header = $('<p>')
-            .text($username);
-        const $footer = $('<p>')
-            .text(tweet.created_at)
-            .addClass('footer');
+const createTweetElement = ((tweet) => {
+    // const $username = $('<h2>').text(tweet.user.name);
+    // const $userhandle = $('<h4>').text(tweet.user.handle);
+    // const $tweetContent = $('<p>').text(tweet.content.text);
+    // const $header = $('<p>')
+    //     .text($username);
+    // const $footer = $('<p>')
+    //     .text(tweet.created_at)
+    //     .addClass('footer');
 
-        const tweetCreatedAt = $('<p>').text(tweet.content.text);
-    })
-}
+    // const tweetCreatedAt = $('<p>').text(tweet.content.text);
+    return `
 
-// console.log(createTweetElement(tweetData));
+    
+    <article class="tweet" id="article-tweet">
+    <header class="tweet-header">
+      <h2>
+      <img id="tweet_image" src="./images/avator_header.png">
+          ${tweet.user.name}
+      </h2>
 
+        <h4>${tweet.user.handle}</h4>    
+    </header>
 
-// const renderTweets = (arrTweetObjects) => {
+  <div>
+    <p>${tweet.content.text}</p>
+  </div>
 
-//     arrTweetObjects.forEach(tweetObj => {
-//         const tweetMarkup = createTweetElement(tweetObj);
-//         const container = $(".container");
-//         console.log(container);
-//         $( ".container" ).append(tweetMarkup);
-//         // $( ".container" ).append("YOOooooooOOOO");
-//     });
-// }
-
-// promise syntax for Ajax .done and .fail instead of .success and
-// they are returning jQuery Promises. 
-// getJSON();
-// Either passing the callback
-// Post.userID. 
-// $.getJSON('')
-
-
-// const submitForm = () => {
-
-
-// }
-
-// const renderTweets = (data) => {
-//     data === Array;
-//     data.forEach(() => {
-//         // 
-//     });
-// };
+    <footer>
+      <p class="timestamp">${tweet.user.created_at}</p>
+      <p class="icons">
+        <i class="fas fa-flag"></i>
+        <i class="fas fa-retweet"></i>
+        <i class="fas fa-heart"></i>
+      </p>
+    </footer>
+  </article>
+    `;
+});
 
 
-    $(() => {
+$(() => {
     console.log('test');
     const $button = $('#tweet-button');
     $button.on('click', function () {
@@ -66,17 +57,30 @@ const createTweetElement = (tweetObj, moutPoint) => {
     });
 })
 
+
+
+
+const renderTweets = (tweets) => {
+    tweets.forEach(tweet => {
+        const tweetMarkup = createTweetElement(tweet);
+        const container = $(".container");
+        console.log(container);
+        $(".container").append(tweetMarkup);
+        // $(".container").append("YOOooooooOOOO");
+    });
+}
+
+
+
 // fetching tweets from the http://localhost:8080/tweets page
 const loadTweets = () => {
-    const $button = $('#load-more-posts');
+
     $.ajax('http://localhost:8080/tweets', { method: 'GET' })
-        .then(function (data) {
-            // let dataJSON = JSON.parse(data);
+        .then((data) => {
             console.log(data);
+            renderTweets(data);
         });
 }
 
+
 loadTweets();
-
-
-
